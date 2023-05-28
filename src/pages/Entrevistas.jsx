939b10b5c9f3c99b5fe1aecc503871a0b87af767
csosorioa <cs.osorioa@uniandes.edu.co>
2023-05-27 20:57:19 -0500

    final commit

--- src/pages/Entrevistas.jsx
+++ src/pages/Entrevistas.jsx
@@ -5,6 +5,7 @@ function Entrevistas() {
         <div className='videos'>
             <div className='videos'>
             <h2 className='x p'>Bastien Rodríguez</h2>
+            <h3>La nueva era del fútbol basado en Data</h3>
             <p className='description'>Analista Táctico y videoanalista graduado de la Federación Francesa de fútbol.<br/>Ha trabajado en los clubes colombianos: América de Cali y Junior de Barranquilla.<br/>Actualmente, se encuentra en el fútbol mexicano, con el equipo Tigres de Monterrey.</p>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/j70vIo_xpk0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
