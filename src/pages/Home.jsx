5d72f8c7231353786305b85096ac1a12c8bd5674
csosorioa <cs.osorioa@uniandes.edu.co>
2023-05-27 20:27:56 -0500

    Commit datos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Video from '../components/Video';
 import Qatar from '../components/Qatar';
-
+import '../assets/images/est1.jpeg';
+import '../assets/images/est2.jpeg';
+import '../assets/css/home.css';
 
 function Home() {
     return(
         <div className='home-div'>
             <Video />
             <Qatar />
+            <img style={{ width: 700, height: 600, display: 'block', margin: "20px auto"}} className='stats' src={require("../assets/images/est1.jpeg")} alt=''/>
+            <img style={{ width: 700, height: 600, display: 'block', margin: "20px auto"}} className='stats' src={require("../assets/images/est2.jpeg")} alt=''/>
         </div>
     );
 }
